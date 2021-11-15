7eb7d9855895f6c30e5d71a7f0ffce8dcb35b92c
RigobertDanon <danonr@wanadoo.fr>
2021-11-15 14:16:47 +0100

    dev layout page index + création fichier style

--- index.php
+++ index.php
@@ -37,5 +37,44 @@
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
     -->
+
+
+  
+        <footer class="bg-dark text-white">
+            <div class="d-flex align-items-center zone-footer">
+                <div class="col-sm-6 col-md-6 zone-administratif">
+                    <h4 class="text-center">Administratifs</h4>
+                    <ul class="d-flex justify-content-center menu-footer mb-0">
+                        <li><a href="">CGV</a></li>
+                        <li><a href="">Mentions légales</a></li>
+                        <li><a href="contact.html">Contact</a></li>
+                    </ul>
+                </div>
+                <div class="col-sm-6 col-md-6">
+                    <h4 class="text-center">Mes réseaux</h4>
+                    <p class="d-flex justify-content-center mb-0">
+                        <a href=""><i class="bi bi-facebook icone-b"></i></a>
+                        <a href=""><i class="bi bi-instagram icone-b"></i></a>
+                        <a href=""><i class="bi bi-twitter icone-b"></i></a>
+                    </p>
+                </div>
+            </div>
+            <div class="d-flex align-items-end div-footer-copyright">
+                <p class="text-center zone-copyright py-3 mb-0">&copy; 2021 | Rigobert DANON</p>
+            </div>
+        </footer>
+
+ 
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
+
+
+    </body>
+</html>        
+
+
+
+
+
+
   </body>
 </html>
